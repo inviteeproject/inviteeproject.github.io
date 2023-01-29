7347164cec7498b107b15b922dce89b79a762f73
odilioarys <odilioarys@gmail.com>
2023-01-29 23:58:43 +0700

    perbaikan

--- khitan-ganesha/js/main.js
+++ khitan-ganesha/js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Jul 25, 2023 10:37:25").getTime();
+		var countDownDate = new Date("Feb 18, 2023 06:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
